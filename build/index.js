71c8e3738b907b8d94f44520ee938c4a80042520
Eesa Abed <essaji44@gmail.com>
2017-10-16 17:06:31 +0400

    initial workable solution

--- build/index.js
+++ build/index.js
@@ -534,7 +534,7 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var DEFAULT_COLOR = "#0087cc";
+var DEFAULT_COLOR = "rgb(147, 204, 232)";
 
 var CardFlippable = function (_Component) {
     _inherits(CardFlippable, _Component);
@@ -619,8 +619,8 @@ var CardFlippable = function (_Component) {
                         { className: backCardClasses },
                         this.props.backChildren || _react2.default.createElement(
                             'div',
-                            { style: { backgroundColor: DEFAULT_COLOR, height: "100%", padding: "10px" } },
-                            'Card back content goes here'
+                            { style: { color: "white", backgroundColor: DEFAULT_COLOR, height: "100%", padding: "10px" } },
+                            'Click to flip again'
                         )
                     )
                 )
---
+++ src/index.js
@@ -0,0 +1,81 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types'
+import "../assets/style.css";
+
+const DEFAULT_COLOR = "rgb(147, 204, 232)";
+
+export default class CardFlippable extends Component {
+
+    constructor() {
+        super();
+        this.state = {isFlipped: false};
+    }
+
+    toggleCard() {
+        if (this.props.cardId === this.props.activeCardId)
+            return this.setState({isFlipped: !this.state.isFlipped});
+
+        this.props.setActiveCardId(this.props.cardId);
+    }
+
+    render() {
+
+        const {title, description, thumbnailUrl, cardId, activeCardId, } = this.props;
+
+        let bgImageStyle = {
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+        };
+
+        if(thumbnailUrl)
+            bgImageStyle["backgroundImage"] = `url(${thumbnailUrl})`;
+        else
+            bgImageStyle["backgroundColor"] = DEFAULT_COLOR;
+
+        const isFlipped = this.state.isFlipped;
+
+        const backCardClasses = ['side', 'alef-card-back'].join(" ");
+        const cardClasses = ["alef-card", isFlipped ? "flip-alef-card" : ""].join(" ");
+
+        return (
+            <div className="alef-card-container">
+                <div onClick={() => this.toggleCard()} className={cardClasses}>
+                    <div className="side">
+                        {this.props.frontChildren || <div>
+                            <div style={bgImageStyle} className="alef-card-bg" >
+                                {title ? <div className="alef-card-title" >{title}</div> : null}
+                            </div>
+                            <div className="alef-card-info" >
+                                <div className="alef-card-description" >
+                                    {description || "Card description goes here"}
+                                </div>
+                            </div>
+                        </div>}
+                    </div>
+                    <div className={backCardClasses}>
+                        {this.props.backChildren || <div style={{color: "white", backgroundColor: DEFAULT_COLOR, height: "100%", padding: "10px"}} >Click to flip again</div>}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+CardFlippable.propTypes = {
+    link: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.object
+    ]),
+    title: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.object
+    ]),
+    description: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.object
+    ]),
+    thumbnailUrl: PropTypes.string,
+    cardId: PropTypes.string,
+    setActiveCardId: PropTypes.func,
+    activeCardId: PropTypes.string
+};
